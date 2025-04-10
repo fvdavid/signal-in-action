398691e0a8382d92e79a5df46188a818233658dd
David Tobing <saddamtbg@gmail.com>
2025-04-10 19:56:03 +0700

    Streams with rxResource

--- src/app/demo/streaming-resource-demo/timer-rx-resource/timer-rx-resource.component.ts
+++ src/app/demo/streaming-resource-demo/timer-rx-resource/timer-rx-resource.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, computed, ResourceStatus, signal } from '@angular/core';
+import { timerrxResource } from '../utils/timer-rxresource';
 
 @Component({
   selector: 'app-timer-rx-resource',
   imports: [],
   templateUrl: './timer-rx-resource.component.html',
-  styleUrl: './timer-rx-resource.component.scss'
+  styleUrl: './timer-rx-resource.component.scss',
 })
 export class TimerRxResourceComponent {
+  ResourceStatus = ResourceStatus;
 
+  startValue = signal(0);
+
+  request = computed(() => ({
+    startValue: this.startValue(),
+  }));
+
+  timerResource = timerrxResource(1000, this.startValue);
+
+  forward(): void {
+    this.startValue.update((v) => nextSegment(v));
+  }
+}
+
+function nextSegment(currentValue: number): number {
+  return Math.floor(currentValue / 100) * 100 + 100;
 }
---
+++ src/app/demo/streaming-resource-demo/utils/timer-rxresource.ts
@@ -0,0 +1,30 @@
+import { computed, ResourceRef } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { interval, map, startWith, switchMap, tap, throwError } from 'rxjs';
+
+export function timerrxResource(
+  timeout: number,
+  startValue: () => number
+): ResourceRef<number | undefined> {
+  const request = computed(() => ({
+    startValue: startValue(),
+  }));
+
+  return rxResource({
+    request: request,
+    loader: (params) => {
+      const startValue = params.request.startValue;
+      return interval(timeout).pipe(
+        map((v) => v + startValue + 1),
+        startWith(startValue),
+        tap((v) => console.log('counter', v)),
+        switchMap((value) => {
+          if (value === 7 || value === 13) {
+            return throwError(() => 'bad luck');
+          }
+          return [value];
+        })
+      );
+    },
+  });
+}
